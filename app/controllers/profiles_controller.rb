93df5b250222f932d3f9c29f069a14a065907b8d
Davidalen Fountain <dfountain@sacent.com>
2018-03-05 02:14:15 +0000

    Fixed logic errors in profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -17,7 +17,7 @@ class ProfilesController < ApplicationController
     @user.settings(:user_show_settings).show_name = true?(params[:show_name])
     @user.settings(:user_show_settings).show_email = true?(params[:show_email])
     @user.settings(:user_show_settings).show_birthday = true?(params[:show_birthday])
-    @user.settings(:user_show_settings).show_website = true?(params[:show_birthday])
+    @user.settings(:user_show_settings).show_website = true?(params[:show_website])
     
     if @user.save()
       flash[:success] = "Profile Updated Successfully"
